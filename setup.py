c9263bf530c69d400b25d05cb413116dfda1fed0
linzhanguca <lzhang12@uca.edu>
2026-02-24 20:23:03 -0600

    update package setup and save rviz config

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import find_packages, setup
-import os
-from glob import glob
+from pathlib import Path
 
 
 package_name = 'homer_navigation'
@@ -14,16 +13,16 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (
-            os.path.join("share", package_name, "configs"),
-            glob(os.path.join("configs", "*.yaml")),
+            str(Path("share") / package_name / "configs"),
+            [str(p) for p in Path("configs").glob("*.yaml")],
         ),
         (
-            os.path.join("share", package_name, "rviz"),
-            glob(os.path.join("rviz", "*.rviz")),
+            str(Path("share") / package_name / "rviz"),
+            [str(p) for p in Path("rviz").glob("*.rviz")],
         ),
         (
-            os.path.join("share", package_name, "launch"),
-            glob(os.path.join("launch", "*.py")),
+            str(Path("share") / package_name / "launch"),
+            [str(p) for p in Path("launch").glob("*.launch.py")],
         ),
    ],
     install_requires=['setuptools'],
