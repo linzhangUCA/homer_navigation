07abab5a0a4bd2caaa89611f4af5dcc1bf715c58
linzhanguca <lzhang12@uca.edu>
2026-02-26 16:04:15 -0600

    update nav rviz config

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
     homerun_package_path = get_package_share_path('homer_navigation')
     slam_config_path = homerun_package_path / 'configs/localization_params.yaml'
     nav_config_path = homerun_package_path / 'configs/nav2_params.yaml'
-    rviz_config_path = homerun_package_path / 'rviz/localization.rviz'
+    rviz_config_path = homerun_package_path / 'rviz/navigation.rviz'
 
     sim_time_arg = DeclareLaunchArgument(
         name='use_sim_time',
